f28344b4ccf1d8203e008adb72ca3df3e8768bee
aparx <undestroy1310@gmail.com>
2024-01-02 04:56:58 +0100

    Fixed s3 client

--- apps/web/src/server/s3/s3client.ts
+++ apps/web/src/server/s3/s3client.ts
@@ -1,9 +1,11 @@
 // TODO
-/*export const S3 = new S3Client({
+import { S3Client } from '@aws-sdk/client-s3';
+
+export const S3 = new S3Client({
   region: 'auto',
   endpoint: `https://${process.env.S3_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
     accessKeyId: process.env.S3_ACCESS_KEY!,
     secretAccessKey: process.env.S3_ACCESS_SECRET!,
   },
-});*/
+});
